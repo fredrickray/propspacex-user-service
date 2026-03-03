e8a67c9053fe60c251917ca59aa3745d444685e4
Dave <davearonmwan@gmail.com>
2026-03-04 00:05:13 +0100

    feat: add gRPC reflection service

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import * as grpc from '@grpc/grpc-js';
+import { ReflectionService } from '@grpc/reflection';
 import { AppDataSource } from '@config/data.source';
 import indexRouter from './v1/route';
 import DotenvConfig from '@config/dotenv.config';
 import { errorHandler, routeNotFound } from '@middlewares/error.middleware';
 import UserServiceImpl from '@grpc/server/user.server';
-import { Protos } from './grpc';
+import { Protos, packageDefinition } from './grpc';
 
 export default class Server {
   public app: Application;
@@ -85,6 +86,9 @@ export default class Server {
         logActivity: userService.logActivity,
         checkDeviceTrust: userService.checkDeviceTrust,
       });
+
+      const reflectionService = new ReflectionService(packageDefinition);
+      reflectionService.addToServer(this.grpcServer);
 
       console.log('gRPC services registered successfully');
     } catch (error) {
